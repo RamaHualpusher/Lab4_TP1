41797b429ee452b1e27af3205e509ff76491dd38
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-03-10 19:22:45 -0300

    Cambio de colores en tabla

--- js/lista.js
+++ js/lista.js
@@ -1,6 +1,8 @@
 //Traer los datos de la API para llenar la tabla
 //Traer los datos en formato json (http://168.194.207.98:8081/tp/lista.php?action=BUSCAR)
 //Llenar la tabla con los datos
+let letStart = 30;
+
 
 function getAll() {
   let url = "http://168.194.207.98:8081/tp/lista.php?action=BUSCAR";
@@ -10,22 +12,40 @@ function getAll() {
       console.log(data);
       let tabla = document.getElementById("content-table");
       tabla.innerHTML = "";
-      for (let i = 0; i < 30; i++) {
+      for (let i = letStart; i < letStart+30; i++) {
+        if(data[i].bloqueado == "Y"){
         tabla.innerHTML += `
-            <div class="row row-item">
-                <div class="table-item">${data[i].id}</div>
-                <div class="table-item">${data[i].usuario}</div>
-                <div class="table-item">${data[i].bloqueado}</div>
-                <div class="table-item">${data[i].apellido}</div>
-                <div class="table-item">${data[i].nombre}</div>
-                <div class="table-item"> <button class="btn btn-success" onclick="bloquear(${data[i].id})">BLOQUEAR</button> </div>
-                <div class="table-item"> <button class="btn btn-danger" onclick="desbloquear(${data[i].id})">DESBLOQUEAR</button> </div>
+            <div class="row row-green" id="row-item-${data[i].id}">
+                <div class="col-item-center">${data[i].id}</div>
+                <div class="col-item">${data[i].usuario}</div>
+                <div class="col-item-center">${data[i].bloqueado}</div>
+                <div class="col-item">${data[i].apellido}</div>
+                <div class="col-item">${data[i].nombre}</div>
+                <div class="col-item-center-btn"> <button class="btn btn-success" onclick="bloquear(${data[i].id})">BLOQUEAR</button> </div>
+                <div class="col-item-center-btn"> <button class="btn btn-danger" onclick="desbloquear(${data[i].id})">DESBLOQUEAR</button> </div>
             </div>    
             `;
+        }else{
+          tabla.innerHTML += `
+            <div class="row row-red" id="row-item-${data[i].id}">
+                <div class="col-item-center">${data[i].id}</div>
+                <div class="col-item">${data[i].usuario}</div>
+                <div class="col-item-center">${data[i].bloqueado}</div>
+                <div class="col-item">${data[i].apellido}</div>
+                <div class="col-item">${data[i].nombre}</div>
+                <div class="col-item-center-btn"> <button class="btn btn-success" onclick="bloquear(${data[i].id})">BLOQUEAR</button> </div>
+                <div class="col-item-center-btn"> <button class="btn btn-danger" onclick="desbloquear(${data[i].id})">DESBLOQUEAR</button> </div>
+            </div>    
+            `;
+        }
       }
     });
 }
 
+function nextPage(){
+  letStart += 30;
+  getAll();
+}
 function bloquear(id) {
   let url =
     "http://168.194.207.98:8081/tp/lista.php?action=BLOQUEAR&idUser=" +
@@ -35,7 +55,6 @@ function bloquear(id) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      //TODO: toggle class
       getAll();
     });
 }
@@ -49,7 +68,7 @@ function desbloquear(id) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      //TODO: toggle class
       getAll();
     });
-}
+  }
+  
