d702b4167891d02403c3144a4f5db07195c2c798
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-03-14 23:50:29 -0300

    Se agrega la funcionalidad Busqueda

--- js/lista.js
+++ js/lista.js
@@ -1,59 +1,36 @@
 //Traer los datos de la API para llenar la tabla
 //Traer los datos en formato json (http://168.194.207.98:8081/tp/lista.php?action=BUSCAR)
 //Llenar la tabla con los datos
-let letStart = 30;
 
+var isVerificado = false;
+
+function verificarSession() {
+  if (sessionStorage.getItem("user") == null) {
+    window.location.href = "login.html";
+  }
+}
 
 function getAll() {
+  if(!isVerificado){
+    verificarSession();
+    isVerificado = true;
+  }
   getUser();
   let url = "http://168.194.207.98:8081/tp/lista.php?action=BUSCAR";
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      let tabla = document.getElementById("content-table");
-      tabla.innerHTML = "";
-      for (let i = letStart; i < letStart+30; i++) {
-        if(data[i].bloqueado == "N"){
-        tabla.innerHTML += `
-            <div class="row row-green" id="row-item-${data[i].id}">
-                <div class="col-item-center">${data[i].id}</div>
-                <div class="col-item">${data[i].usuario}</div>
-                <div class="col-item-center">${data[i].bloqueado}</div>
-                <div class="col-item">${data[i].apellido}</div>
-                <div class="col-item">${data[i].nombre}</div>
-                <div class="col-item-center"> <button class="btn btn-danger" onclick="bloquear(${data[i].id})"><i class="fa-solid fa-lock"></i></button> </div>
-                <div class="col-item-center"> <button class="btn btn-success" onclick="desbloquear(${data[i].id})"><i class="fa-solid fa-lock-open"></i></button> </div>
-            </div>    
-            `;
-        }else{
-          tabla.innerHTML += `
-            <div class="row row-red" id="row-item-${data[i].id}">
-                <div class="col-item-center">${data[i].id}</div>
-                <div class="col-item">${data[i].usuario}</div>
-                <div class="col-item-center">${data[i].bloqueado}</div>
-                <div class="col-item">${data[i].apellido}</div>
-                <div class="col-item">${data[i].nombre}</div>
-                <div class="col-item-center-btn"> <button class="btn btn-danger" onclick="bloquear(${data[i].id})"><i class="fa-solid fa-lock"></i></button> </div>
-                <div class="col-item-center-btn"> <button class="btn btn-success" onclick="desbloquear(${data[i].id})"><i class="fa-solid fa-lock-open"></i></button> </div>
-            </div>    
-            `;
-        }
-      }
+      llenarTabla(data);
     });
 }
 
-function getUser(){
+function getUser() {
   const subtitle = document.getElementById("sub-title").value;
   const user = sessionStorage.getItem("user").toUpperCase();
   console.log(user);
   document.getElementById("sub-title").innerHTML = `Bienvenido ${user}`;
 }
 
-function nextPage(){
-  letStart += 30;
-  getAll();
-}
 function bloquear(id) {
   let url =
     "http://168.194.207.98:8081/tp/lista.php?action=BLOQUEAR&idUser=" +
@@ -78,5 +55,55 @@ function desbloquear(id) {
       console.log(data);
       getAll();
     });
+}
+
+function buscar() {
+  const busqueda = document.getElementById("buscarUser").value;
+  let url =
+    "http://168.194.207.98:8081/tp/lista.php?action=BUSCAR&usuario=" + busqueda;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      llenarTabla(data);
+    });
+}
+
+function llenarTabla(data) {
+  let tabla = document.getElementById("content-table");
+  tabla.innerHTML = "";
+  if (data.length == 0) {
+    tabla.innerHTML = `<div class="row-red">
+    <div class="mje-error">No se encontraron resultados</div>
+    </div>`;
+  } else {
+    //Sólo pinto hasta los primeros 30 registros por rendimiento.
+    //Voy a seguir investigando cómo paginar los resultados para que no se vea tan feo.
+    for (let i = 0; i < data.length && i < 30; i++) {
+      if (data[i].bloqueado == "N") {
+        tabla.innerHTML += `
+        <div class="row row-green" id="row-item-${data[i].id}">
+            <div class="col-item-center">${data[i].id}</div>
+            <div class="col-item">${data[i].usuario}</div>
+            <div class="col-item-center">${data[i].bloqueado}</div>
+            <div class="col-item">${data[i].apellido}</div>
+            <div class="col-item">${data[i].nombre}</div>
+            <div class="col-item-center"> <button class="btn btn-danger" onclick="bloquear(${data[i].id})"><i class="fa-solid fa-lock"></i></button> </div>
+            <div class="col-item-center"> <button class="btn btn-success" onclick="desbloquear(${data[i].id})"><i class="fa-solid fa-lock-open"></i></button> </div>
+        </div>    
+        `;
+      } else {
+        tabla.innerHTML += `
+        <div class="row row-red" id="row-item-${data[i].id}">
+            <div class="col-item-center">${data[i].id}</div>
+            <div class="col-item">${data[i].usuario}</div>
+            <div class="col-item-center">${data[i].bloqueado}</div>
+            <div class="col-item">${data[i].apellido}</div>
+            <div class="col-item">${data[i].nombre}</div>
+            <div class="col-item-center-btn"> <button class="btn btn-danger" onclick="bloquear(${data[i].id})"><i class="fa-solid fa-lock"></i></button> </div>
+            <div class="col-item-center-btn"> <button class="btn btn-success" onclick="desbloquear(${data[i].id})"><i class="fa-solid fa-lock-open"></i></button> </div>
+        </div>    
+        `;
+      }
+    }
   }
-  
+}
